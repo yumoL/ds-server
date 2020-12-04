04dba60563e083f134fb9c102e50756f11ba0f02
songxingchang <52391758+songxingchang@users.noreply.github.com>
2020-12-04 19:42:34 +0200

    backup in definite time

---
+++ redis_to_db/backup.py
@@ -0,0 +1,26 @@
+import time
+import os
+import sys
+
+start_time = sys.argv[1]  
+timeArray = time.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+start = int(time.mktime(timeArray))
+
+#back up time lag: 300 (s)
+backup_timelag = 100
+
+while True:
+    try:
+        cur = time.time()
+        if cur - start >= backup_timelag:
+            os.system("mysqldump -u root -ppassword dsp > ~/dsp_backup/dsp.sql")
+            start = time.time()
+
+        else:
+            time.sleep(backup_timelag)
+    except:
+        print("backup error...check!")
+        time.sleep(5)
+        
+# usage: type in command line like this:
+# python3 backup.py "2020-12-4 19:36:00"
